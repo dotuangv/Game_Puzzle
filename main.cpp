c4972cacbf276bb887a4c895540c5c9edc673e53
vulinhnopro2704 <vulinhtruong79@gmail.com>
2023-09-01 21:31:21 +0700

    NEW UPDATE

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-#include  <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#include <SDL_mixer.h>
 #include "Game.h"
 
 using namespace std;
@@ -9,14 +5,11 @@ Game* game;
 
 int main(int argc, char* argv[])
 {
-    const int FPS = 60;
-    const int frameDelay = 1000 / FPS;
-
     Uint32 frameStart;
     int frameTime;
 
     game = new Game();
-    game->init("PUZZLE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+    game->init("PUZZLE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, false);
     while (game->running()) {
         frameStart = SDL_GetTicks();
 
