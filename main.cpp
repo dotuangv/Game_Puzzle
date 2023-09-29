736ee913444142961631d8cc2d6a463341768718
dotuangv <dovantuan.gv2004@gmail.com>
2023-09-29 14:16:23 +0700

    nothing

--- main.cpp
+++ main.cpp
@@ -13,31 +13,31 @@ SDL_Renderer* gRenderer = nullptr;
 int main(int argc, char* argv[])
 {
     
-    //start = new Start(4);
-    //start->StartInit("PUZZLE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, Width, Height, false);
-    //start->SetStart();
-    //game = new Gameplay();
-    //game->init("PUZZLE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, false);
-    //while (game->GetRunning2())
-    //{
-    //    game->SetStart();
-    //    game->StartUpdate();
-    //    game->StartRenderer();
-    //    game->StartEvents();
-    //    //cout << "HEllo\n";
-    //    if (!game->GetRunning2())
-    //    {
-    //        game->SetUpGame(SCREEN_HEIGHT);
-    //        //game->Play();
-    //    }
-    //}
-    ////game->SetUpGame(Height);
-    ////game->Play();
-    ////game->handleEvents();
-    //game->SolveGame();
+    start = new Start(4);
+    start->StartInit("PUZZLE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, Width, Height, false);
+    start->SetStart();
+    game = new Gameplay();
+    game->init("PUZZLE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, false);
+    while (game->GetRunning2())
+    {
+        game->SetStart();
+        game->StartUpdate();
+        game->StartRenderer();
+        game->StartEvents();
+        //cout << "HEllo\n";
+        if (!game->GetRunning2())
+        {
+            game->SetUpGame(SCREEN_HEIGHT);
+            //game->Play();
+        }
+    }
+    //game->SetUpGame(Height);
+    //game->Play();
+    //game->handleEvents();
+    game->SolveGame();
     //game->clean();
     //game->Clear();
-    MainMenu a;
-    a.run();
+    //MainMenu a;
+    //a.run();
 	return 0;
 }
